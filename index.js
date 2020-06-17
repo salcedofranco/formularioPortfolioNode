4548cb0f14951b7d4e251530b33b0197b91e40d8
franco <fcedo13@gmail.com>
2020-06-17 11:02:55 -0300

    first

---
+++ index.js
@@ -0,0 +1,105 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(require('./routes/index'));
+
+const sendMail = require('./routes/index');
+
+require('dotenv').config()
+
+
+// Sirve archivos estáticos (lo necesitamos para importar un archivo CSS) 
+app.use(express.static('public'))
+
+// Carga el módulo de manillar 
+const handlebars = require('express-handlebars');
+
+// Configura nuestra aplicación para usar el motor del manillar 
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({
+  extended:false
+}));
+app.use(express.json());
+
+//email, subject, text
+app.post('/email', (req, res) => {
+  //TODO:
+  //send email here
+  const { subject, email, text } = req.body;
+  console.log('Data: ', req.body);
+
+  sendMail(email, subject, text, function(err, data) {
+      if (err) {
+          res.status(500).json({ message: 'Error'});
+      } else {
+          res.json({ message: 'Email enviado!'})
+      }
+  });
+  
+});
+
+// Establece las configuraciones del manillar 
+//app.engine('hbs', handlebars({
+   //layoutsDir: __dirname + '/views/layouts',
+    //new configuration parameter
+    //extname: 'hbs'
+    //}));
+
+    //ACA LLAMAMOS A PLANB.HBS
+    app.engine('hbs', handlebars({
+        layoutsDir: __dirname + '/views/layouts',
+        extname: 'hbs',
+        //new configuration parameter
+        defaultLayout: 'planB',
+        //new configuration parameter
+        partialsDir: __dirname + '/views/partials/'
+        }));
+
+        //I would like to use a real api but let's use this for the sake of //the simplicity of the article
+
+      
+
+
+        //ACA LLAMAMOS A INDEX.HBS
+//app.get('/', (req, res) => { 
+// Sirve el cuerpo de la página aka "main.handlebars" al contenedor // aka "index.handlebars" 
+//res.render('main', {layout : 'index'});
+//});
+
+        //ACA LLAMAMOS A planb
+//app.get('/', (req, res) => {
+    //instead of res.render('main', {layout: 'index'});
+    //res.render('main');
+    //});
+
+
+      //app.get('/dinamica', (req, res) => {
+        //res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true});
+        //});
+
+    app.get('/', (req, res) => {
+        //Using the index.hbs file instead of planB
+        res.render('main', {layout: 'index'});
+    });
+
+
+    //ARCHIVOS DINAMICOS, PASAMOS OBJETOS
+    
+//app.get('/', (req, res) => {
+//res.render('main', {layout: 'index', proPlayer: fakeApi()});
+//});
+
+
+// Establece una ruta básica 
+//app.get('/', (req, res) => res.send('Hello World !'));
+
+/** Puerto y host para la app */
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => {
+    console.log('El servidor esta funcionando')
+});
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+const { Router } = require('express');
+const router = Router();
+
+const nodemailer = require('nodemailer');
+
+
+require('dotenv').config()
+
+
+
+transporter = nodemailer.createTransport({
+    host: 'ca9.toservers.com',
+    port: process.env.MI_PORT,
+    secure: true,
+    auth: {
+        user: 'contact@francosalcedodev.com.ar',
+        pass: process.env.MI_CONTRA,
+        
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+const sendMail = (email, subject, text, cb) => {
+    const mailOptions = {
+        from: '"DESDE PORTFOLIO" <contact@francosalcedodev.com.ar>',
+        to: process.env.MI_EMAIL,
+        subject,
+        text
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+            cb(err, null);
+        } else {
+            cb(null, data);
+        }
+    });
+
+}
+
+
+module.exports = sendMail;
