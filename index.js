76672cc1f4a255ea07ba26749f1df9b60b6388ff
franco <fcedo13@gmail.com>
2020-06-21 22:54:56 -0300

    la

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 
 const express = require('express');
-var http = require('http');
-const enforce = require('express-sslify');
+
 const app = express();
 
-//HTTPS redirect middleware
-function ensureSecure(req, res, next) {
-    //Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
-    if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost') {
-        //Serve Angular App by passing control to the next middleware
-        next();
-    } else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
-        //Redirect if not HTTP with original request URL
-        res.redirect('https://' + req.hostname + req.url);
-    }
-}
-
-app.use('/', ensureSecure);
 
 const path = require('path');
 
